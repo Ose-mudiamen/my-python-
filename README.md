cda187e81f4e7b69bf7aa2dc560b19c3782161da
Ose-mudiamen <osezuabenjamin50@gmail.com>
2026-04-11 22:03:24 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+My Python learning journey. Contains beginner projects as I learn Python for AI engineering.
--- names.py
+++ names.py
@@ -5,5 +5,10 @@ file.write("Job\n")
 file.write("Steve\n")
 file.write("Jota\n")
 
+file = open("Names.txt","a")
+newname = input("Enter new name: ")
+file.write(newname + "\n")
+
 file = open("Names.txt","r")
 print(file.read())
+file.close
